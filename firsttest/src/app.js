efba96b7e34b67490d5b07b44dfedf8fa098c5ee
RufaydaEqailan <rufayda.family@gmail.com>
2023-10-02 21:39:04 +0200

    promise resolve

--- firsttest/src/app.js
+++ firsttest/src/app.js
@@ -20,7 +20,7 @@ const App = () => {
     let myPromise = new Promise((success, error) => {
       const x = 0;
       if (x === 0) {
-        success("This is ok");
+        success("This is success");
       } else {
         error("This is fail");
       }
