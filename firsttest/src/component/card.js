4582066fb04a8a5c7180f4259147e1a2e091d73c
RufaydaEqailan <rufayda.family@gmail.com>
2023-10-03 21:05:31 +0200

    props array

--- firsttest/src/component/card.js
+++ firsttest/src/component/card.js
@@ -1,17 +1,13 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import img2 from "../images/img2.webp";
 
 const CardDetails = (props) => {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={img2} />
+      <Card.Img variant="top" src={props.img} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Text>{props.description}</Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
--- firsttest/src/content.js
+++ firsttest/src/content.js
@@ -1,19 +1,41 @@
 import React from "react";
 import CardDetails from "./component/card";
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.webp";
+import img3 from "./images/img3.png";
+import img4 from "./images/img4.jpeg";
 
-const title1 = "this is title 1";
-const title2 = "this is title 2";
-const title3 = "this is title 3";
-const title4 = "this is title 4";
+// const title1 = "this is title 1";
+// const title2 = "this is title 2";
+// const title3 = "this is title 3";
+// const title4 = "this is title 4";
+// const Content = () => {
+//   return (
+//     <div>
+//       <CardDetails title={title1} />
+//       <CardDetails title={title2} />
+//       <CardDetails title={title3} />
+//       <CardDetails title={title4} />
+//     </div>
+//   );
+// };
+const data = [
+  { title: "title1", description: "desc1", img: img1 },
+  { title: "title2", description: "desc2", img: img2 },
+  { title: "title3", description: "desc3", img: img3 },
+  { title: "title4", description: "desc4", img: img4 },
+];
 const Content = () => {
-  return (
-    <div>
-      <CardDetails title={title1} />
-      <CardDetails title={title2} />
-      <CardDetails title={title3} />
-      <CardDetails title={title4} />
-    </div>
-  );
+  return data.map((item, index) => {
+    return (
+      <CardDetails
+        title={item.title}
+        description={item.description}
+        img={item.img}
+        key={index}
+      />
+    );
+  });
 };
 
 export default Content;
